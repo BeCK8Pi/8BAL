3f6864abd894780443a0e81dc6a1ada444e301d9
BeCK8Pi <khuntip1207@gmail.com>
2023-12-12 16:24:09 +0700

    finish

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,53 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  float age, height;
+  long int bounty;
+  string name;
+
+  cout << "Enter your age: ";
+  cin >> age;
+
+  if( age <= 25){
+    cout << "Enter your height: ";
+    cin >> height;
+    if( height < 100){
+      name = "Chopper";
+    }
+    else if( height < 180){
+      name = "Usopp";
+    }
+    else{
+      cout << "Enter your bounty: ";
+      cin >> bounty;
+      if( bounty > 1100000000){
+        name = "Zoro";
+      }
+      else{
+        name = "Sanji";
+      }
+    }
+  }
+  else{
+    if( age > 60){
+        name = "Brook";
+      }
+    else{
+      cout << "Enter your bounty: ";
+      cin >> bounty;
+      if( bounty > 500000000){
+        name = "Jinbe";
+      }
+      else{
+        name = "Franky";
+      }
+    }
+  }
+
+  cout << "Your character = " << name;
+
+  return 0;
+}
